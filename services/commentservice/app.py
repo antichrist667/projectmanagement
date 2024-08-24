1f940eaea1bc2654bfa8e0ce4e499a50a432b77e
antichrist667 <162932770+antichrist667@users.noreply.github.com>
2024-08-24 13:13:52 -0500

    fix: :bug: add Cors in comment service, dockerfile, and requeriments

--- services/commentservice/app.py
+++ services/commentservice/app.py
@@ -1,9 +1,13 @@
 import os
 from flask import Flask
+from flask_cors import CORS  
 from flask_graphql import GraphQLView
 from graphql_schema import schema
 
 app = Flask(__name__)
+
+
+CORS(app, origins=["https://frontend-zondeli7dq-uc.a.run.app"])
 
 app.add_url_rule(
     '/comments',
