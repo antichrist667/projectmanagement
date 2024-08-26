bac3f132eeec26891c73a44d59c9625b2113afa8
antichrist667 <162932770+antichrist667@users.noreply.github.com>
2024-08-25 23:42:05 -0500

    fix: :bug: Cors allowedHeaders: ['Content-Type', 'Authorization']

--- services/login/server.js
+++ services/login/server.js
@@ -6,20 +6,25 @@ const routes = require('./routes');
 
 const app = express();
 
+// Configuración CORS
 const corsOptions = {
-  origin: 'https://frontend-zondeli7dq-uc.a.run.app',
-  methods: ['GET', 'POST', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  optionsSuccessStatus: 200
+  origin: '*', // Permitir cualquier origen
+  methods: ['GET', 'POST', 'OPTIONS'], // Métodos permitidos
+  allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
+  optionsSuccessStatus: 200 // Código de éxito para las solicitudes OPTIONS
 };
 
+// Aplicar configuración CORS
 app.use(cors(corsOptions));
+
+// Middleware para procesar cuerpos JSON
 app.use(express.json());
 
+// Rutas
 app.use('/api', routes);
 
+// Puerto
 const PORT = process.env.PORT || 3006;
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
